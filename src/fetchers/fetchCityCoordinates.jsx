d34c535f651693e6d528e209403b5bb8dd6fefd1
duc <s3952670@student.rmit.edu>
2024-07-16 16:54:00 +1000

    update http to https

--- src/fetchers/fetchCityCoordinates.jsx
+++ src/fetchers/fetchCityCoordinates.jsx
@@ -1,6 +1,6 @@
 const fetchCityCoordinates = async (city, state, country) => {
   try {
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=5&appid=edb33546896b6ad3e1d57b86d9c0c8db`;
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=5&appid=edb33546896b6ad3e1d57b86d9c0c8db`;
     const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error('Network response was not ok.');
